bfa7b44cdf5066fdcb0328c64eeff71cf5c9b989
sam-hamilton <sam@4ds.co.uk>
2020-02-22 11:33:10 +0000

    Block Lab template for Gov.uk style highlight card

---
+++ blocks/block-gov-uk-style-highlight-card.php
@@ -0,0 +1,4 @@
+<a href="<?php block_field('url'); ?>" title="<?php block_field('title'); ?>" target="<?php block_field('new-window'); ?>" class="custom-subpage-card">
+	<h3><?php block_field('title'); ?></h3>
+	<p><?php block_field('description'); ?></p>
+</a>
