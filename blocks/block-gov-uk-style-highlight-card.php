1db1ca6a04c6e4a3a8a95976f8ddaad97c402d4d
sam-hamilton <sam@4ds.co.uk>
2020-03-12 20:28:58 +0000

    changing titles to h2

--- blocks/block-gov-uk-style-highlight-card.php
+++ blocks/block-gov-uk-style-highlight-card.php
@@ -1,4 +1,4 @@
 <a href="<?php block_field('url'); ?>" title="<?php block_field('title'); ?>" target="<?php block_field('new-window'); ?>" class="custom-subpage-card">
-	<h3><?php block_field('title'); ?></h3>
+	<h2><?php block_field('title'); ?></h2>
 	<p><?php block_field('description'); ?></p>
 </a>
